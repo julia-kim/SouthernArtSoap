77418e0ac32e0756754b0785e769e5b196baed16
Julia Kim <26291536+julia-kim@users.noreply.github.com>
2020-12-03 22:26:18 -0500

    Initial commit

---
+++ src/main/java/com/southernartsoap/SouthernArtSoapApplication.java
@@ -0,0 +1,13 @@
+package com.southernartsoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SouthernArtSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SouthernArtSoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/southernartsoap/SouthernArtSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.southernartsoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SouthernArtSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
