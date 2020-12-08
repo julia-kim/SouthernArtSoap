6ad982460f36ba5799f3019f6839fbd2d9c37880
Julia Kim <26291536+julia-kim@users.noreply.github.com>
2020-12-08 18:20:34 -0500

    Create AuthenticationController

---
+++ src/main/java/com/southernartsoap/controller/AuthenticationController.java
@@ -0,0 +1,44 @@
+package com.southernartsoap.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.southernartsoap.model.User;
+import com.southernartsoap.service.UserService;
+
+@Controller
+class AuthenticationController {
+  @Autowired
+  private UserService userService;
+
+  @GetMapping("/signin")
+  public String login() {
+      return "signin";
+  }
+
+  @PostMapping("/signin")
+  public String singup(@Valid User user,
+                       @RequestParam String submit,
+                       BindingResult bindingResult,
+                       HttpServletRequest request) throws ServletException {
+      String password = user.getPassword();
+      if(submit.equals("up")) {
+          if(userService.findByUsername(user.getUsername()) == null) {
+              userService.saveNew(user);
+          } else {
+              bindingResult.rejectValue("username", "error.user", "Username is already taken.");
+              return "signin";
+          }
+      }
+      request.login(user.getUsername(), password);
+      return "redirect:/";
+  }
+}
