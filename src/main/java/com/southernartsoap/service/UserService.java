581e06d9d149cf8b218448be7066e69ab620f4e5
Julia Kim <26291536+julia-kim@users.noreply.github.com>
2020-12-08 16:24:31 -0500

    Create UserService

---
+++ src/main/java/com/southernartsoap/service/UserService.java
@@ -0,0 +1,53 @@
+package com.southernartsoap.service;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.southernartsoap.model.Product;
+import com.southernartsoap.model.User;
+import com.southernartsoap.repository.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public void saveNew(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+    public void saveExisting(User user) {
+        userRepository.save(user);
+    }
+
+    public User getLoggedInUser() {
+        return findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+    }
+
+    public void updateCart(Map<Product, Integer> cart) {
+        User user = getLoggedInUser();
+        user.setCart(cart);
+        saveExisting(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = findByUsername(username);
+        if(user == null) throw new UsernameNotFoundException("Username not found.");
+        return user;
+    }
+}
