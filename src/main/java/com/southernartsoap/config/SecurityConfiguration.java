38767056c793b9fd34e292cdcdddf7b79a405665
Julia Kim <26291536+julia-kim@users.noreply.github.com>
2020-12-08 16:16:16 -0500

    Add security and mvc configurations

--- src/main/java/com/southernartsoap/config/SecurityConfiguration.java
+++ src/main/java/com/southernartsoap/config/SecurityConfiguration.java
@@ -1,5 +1,42 @@
 package com.southernartsoap.config;
 
-public class SecurityConfiguration {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import com.southernartsoap.service.UserService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.
+		authorizeRequests().antMatchers("/cart").authenticated().and()
+		.formLogin().loginPage("/signin")
+		.loginProcessingUrl("/login").and()
+		.logout().logoutRequestMatcher(new AntPathRequestMatcher("/signout"))
+		.logoutSuccessUrl("/");
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**", "/static/**", "/webjars/**", "/css/**", "/js/**", "/images/**");
+	}
 }
