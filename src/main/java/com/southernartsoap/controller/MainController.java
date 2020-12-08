b48c51c88f04170baf3c9cdfbdbc28ec09e4241d
Julia Kim <26291536+julia-kim@users.noreply.github.com>
2020-12-08 18:13:47 -0500

    Add methods for MainController (from slides)

--- src/main/java/com/southernartsoap/controller/MainController.java
+++ src/main/java/com/southernartsoap/controller/MainController.java
@@ -1,16 +1,54 @@
 package com.southernartsoap.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.southernartsoap.model.Product;
+import com.southernartsoap.service.ProductService;
+
+import lombok.Data;
+
+@Data
+@Controller
+@ControllerAdvice // This makes the `@ModelAttribute`s of this controller available to all controllers, for the navbar.
 public class MainController {
-	@GetMapping(value = "/")
-	public String home() {
-		return "index";
-	}
+    @Autowired
+    ProductService productService;
 
-	@GetMapping(value = "/about")
-	public String aboutUs() {
-		return "about";
-	}
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
 
+    @ModelAttribute("products")
+    public List<Product> products() {
+        return productService.findAll();
+    }
+
+    @ModelAttribute("categories")
+    public List<String> categories() {
+        return productService.findDistinctCategories();
+    }
+
+    @GetMapping("/filter")
+    public String filter(@RequestParam(required = false) String category,
+                         Model model) {
+        List<Product> filtered = productService.findByCategory(category);
+        model.addAttribute("products", filtered); // Overrides the @ModelAttribute above.
+        return "main";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
 }
+
+
