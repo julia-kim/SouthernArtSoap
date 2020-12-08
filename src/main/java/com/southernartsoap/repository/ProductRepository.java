1928141f4c383240317ef87a337130c6073b1a1c
Julia Kim <26291536+julia-kim@users.noreply.github.com>
2020-12-08 16:25:16 -0500

    Update ProductRepository

--- src/main/java/com/southernartsoap/repository/ProductRepository.java
+++ src/main/java/com/southernartsoap/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package com.southernartsoap.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,12 @@ import com.southernartsoap.model.Product;
 
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
-    List<Product> findAll();
-    Product findById(long id);
+	List<Product> findAll();
+
+	Product findById(long id);
+
+	List<Product> findByCategory(String category);
+
+	@Query("SELECT DISTINCT p.category FROM Product p")
+	List<String> findDistinctCategories();
 }
