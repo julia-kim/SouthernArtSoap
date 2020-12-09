108bbfc67cf7d88bcbf7086416c3ab66b045ad5c
Julia Kim <26291536+julia-kim@users.noreply.github.com>
2020-12-09 12:46:50 -0500

    Change db schema to include datatypes

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,2 +1,13 @@
-CREATE TABLE IF NOT EXISTS `product` AS SELECT * FROM CSVREAD('classpath:soaps.csv');
+CREATE TABLE IF NOT EXISTS `product` (
+    product_id bigint primary key, 
+    product_name varchar(255), 
+    price double,
+    description varchar(2000),
+	materials varchar(255),
+	dimensions varchar(255),
+    weight double,
+    etsy_url varchar(1000),
+    category varchar(255)
+)
+AS SELECT * FROM CSVREAD('classpath:soaps.csv');
 CREATE TABLE IF NOT EXISTS `image` AS SELECT * FROM CSVREAD('classpath:images.csv');
